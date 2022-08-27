d07420924ee794765c86a9169b865934b89544e8
wifi-cam <91116979+wifi-cam@users.noreply.github.com>
2022-08-27 10:10:29 -0700

    User choice is asked over 5 rounds and is stored in the variable. Need to fix issue with capitalization and also keep track of wins and losses.

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 let computerChoice;
-let userChoice = '';
+let userChoice;
 
 function getComputerChoice () {
     // Roll random number 1-3 and assign to i
     let shoot = Math.floor(Math.random() * 3); // Random # 1 - 3
     if (shoot == 1) {
-        console.log('Rock!'); // If i == 1 display 'Rock'
+        //console.log('Rock!'); // If i == 1 display 'Rock'
         computerChoice = 'rock'; 
     } else if (shoot == 2) {
-        console.log('Paper!'); // ElseIf i == 2 display 'Paper'
+        //console.log('Paper!'); // ElseIf i == 2 display 'Paper'
         computerChoice = 'paper';
     } else {
-        console.log('Scissors!'); // Else display 'Scissors'
+        //console.log('Scissors!'); // Else display 'Scissors'
         computerChoice = 'scissors';
     }   
 }
 
+function getUserChoice() {
+    let shoot = prompt("Enter Rock, Paper, or Scissors: ");
+    if (shoot == 'rock') {
+        //console.log('Rock!'); // If i == 1 display 'Rock'
+        userChoice = 'rock'; 
+    } else if (shoot == 'paper') {
+        //console.log('Paper!'); // ElseIf i == 2 display 'Paper'
+        userChoice = 'paper';
+    } else if (shoot == 'scissors') {
+        //console.log('Scissors!'); // Else display 'Scissors'
+        userChoice = 'scissors';
+    } else {
+        console.log('Invalid Choice');
+    }
+}
+
 function playRound(userChoice, computerChoice) {
     // Get user choice and store it
-    userChoice = prompt("Enter Rock, Paper, or Scissors: ");
-    userChoice = userChoice.toUpperCase();
+    getUserChoice();
     // getComputerChoice()
     getComputerChoice();
     // Set parameters for winning and losing combinations
@@ -28,8 +43,8 @@ function playRound(userChoice, computerChoice) {
 
 function game() {
     for (let i = 0; i < 5; i++) { //5 Rounds
-
+        playRound();
     }
 }
 
-playRound();
+game();
