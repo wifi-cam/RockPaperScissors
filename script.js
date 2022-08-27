b9ae09edd01788c17f86f743a87572fa4f7838bd
wifi-cam <91116979+wifi-cam@users.noreply.github.com>
2022-08-27 09:47:10 -0700

    Random # generator was only giving 2 and 3, fixed it so it should also give a value of 1

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-let shoot;
 let computerChoice;
-let userChoice;
+let userChoice = '';
 
 function getComputerChoice () {
     // Roll random number 1-3 and assign to i
-    shoot = Math.floor(Math.random() * 3) + 1; // Random # 1 - 3
+    let shoot = Math.floor(Math.random() * 3); // Random # 1 - 3
     if (shoot == 1) {
         console.log('Rock!'); // If i == 1 display 'Rock'
         computerChoice = 'rock'; 
@@ -14,17 +13,13 @@ function getComputerChoice () {
     } else {
         console.log('Scissors!'); // Else display 'Scissors'
         computerChoice = 'scissors';
-    }
-    
-    
-    
+    }   
 }
 
 function playRound(userChoice, computerChoice) {
     // Get user choice and store it
-    userChoice = window.prompt("Enter Rock, Paper, or Scissors: ");
-    userChoice.toLowerCase();
-    userChoice.charAt(0).toUpperCase().slice(1);
+    userChoice = prompt("Enter Rock, Paper, or Scissors: ");
+    userChoice = userChoice.toUpperCase();
     // getComputerChoice()
     getComputerChoice();
     // Set parameters for winning and losing combinations
